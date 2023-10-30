dbeb098d364cc3bba1414bf11faa032437d45f4b
lydiano <andrianina.lydiano@gmail.com>
2023-10-30 17:23:30 +0300

    Make the todo item render as an accordion

--- src/components/Todo/todo-item.jsx
+++ src/components/Todo/todo-item.jsx
@@ -4,11 +4,13 @@ import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import ReplyAllIcon from "@mui/icons-material/ReplyAll";
 import "./todo-item.css";
 
-export const TodoItem = ({ task }) => {
+export const TodoItem = ({ task, onClickTodoTask }) => {
   return (
-    <Paper elevation={3}>
+    <Paper elevation={3} onClick={() => onClickTodoTask()}>
       <div className="task-title">{task.title}</div>
-      <div className="task-description">{task.description}</div>
+      <div className="task-description" hidden={task.hide}>
+        {task.description}
+      </div>
       <div className="task-actions">
         <IconButton aria-label="delete">
           <DeleteIcon />
